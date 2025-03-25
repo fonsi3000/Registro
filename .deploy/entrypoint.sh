e6aafd32590d852fad3cfaf30f59f44ced46af04
fonsi3000 <luiiscarrascal@gmail.com>
2025-03-24 20:48:26 -0500

    Dokerizando el proyecto

--- .deploy/entrypoint.sh
+++ .deploy/entrypoint.sh
@@ -1,26 +1,21 @@
 #!/bin/bash
 
-echo "🎬 entrypoint.sh: [$(whoami)] [PHP $(php -r 'echo phpversion();')]"
-
 cd /var/www/html
 
-# Instalar dependencias
-composer install --no-interaction --optimize-autoloader --no-dev
-composer require laravel/octane --no-interaction
+# Instalar dependencias si es necesario
+if [ "$APP_ENV" != "local" ]; then
+    composer install --optimize-autoloader --no-dev
+fi
 
-# Generar key y optimizar
+# Generar clave y optimizar
 php artisan key:generate --force
 php artisan config:cache
 php artisan route:cache
+php artisan view:cache
 
-# Compilar assets frontend
-npm install
-npm run build
-rm -rf node_modules
-
-# Establecer permisos
-chown -R www-data:www-data /var/www/html
+# Otorgar permisos
+chown -R www:www /var/www/html
 chmod -R 775 storage bootstrap/cache
 
-# Iniciar servicios con Supervisor
+# Iniciar supervisor (que maneja Nginx y Octane)
 exec /usr/bin/supervisord -n -c /etc/supervisor/conf.d/supervisord.conf
