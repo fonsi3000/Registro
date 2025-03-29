c3f063cae3fc04e28221c0eb733f51071f1780d5
fonsi3000 <luiiscarrascal@gmail.com>
2025-03-29 18:01:19 -0500

    Deploy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Forzar el esquema y la URL raíz para todas las URLs generadas por Laravel
+        URL::forceScheme('http');
+        URL::forceRootUrl(config('app.url'));
     }
 }
